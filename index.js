cd5b784988695268fddec93572cb8c94dfca540f
NICO GARCIA <07nico.garcia@gmail.com>
2025-03-21 12:20:21 +0100

    up to date

--- index.js
+++ index.js
@@ -9,23 +9,76 @@ hamMenu.addEventListener("click", () => {
     offScreenMenu.classList.toggle("active");
 })
 
-// query selector pour le bouton ok
-// query selector pour l'input pour récupérer le nom saisi avec .value
-// query selector pour l'onglet name sur la page de jeu
-//function quand le bouton est cliqué pour l'étape 2 et 3 
+// ------------------  Global Variables   ------------------
 
-// function récupérant le nom de l'utilisateur pour la page de jeu quand le bouton ok est cliqué
 
-const validationButton = document.querySelector(".validation");
+// Object Question init:
 
-validationButton.addEventListener("click", function () {
-    const userNameSelector = document.querySelector("input");
-    const userNameInGame = document.querySelector("#user-name");
+const questions = [
+    {
+        question: "Quelle est la capitale de la France ?",
+        answers: ["Paris", "Londres", "Berlin", "Rome"],
+        correctAnswer: "Paris",
+        image: "/assets/images/voyage.jpg",
+    },
+    {
+        question: "Quelle est la capitale de l'Italie'?",
+        answers: ["Marseille", "Rome", "Berlin", "Lisbonne"],
+        correctAnswer: "Rome",
+        image: "/assets/images/culture.jpg",
+    },
+    {
+        question: "Quelle est la capitale de l'Espagne'?",
+        answers: ["Obiwan Kenobi", "Madrid", "Berlin", "Pau"],
+        correctAnswer: "Madrid",
+        image: "/assets/images/quizz.jpg",
+    },
 
-    if (!userNameSelector.value) {
-        alert("Merci de saisir un nom pour commencer le quiz !");
-    }
-    console.log(userNameSelector.value);
+]
 
-    userNameInGame.textContent = userNameSelector.value;
-});
+
+// currentQuestion init: used to display the current question index => starts at 1 
+// and used in displayQuestion() as a parameter as an index (value -1) to access the current question object.
+let currentQuestion = 1;
+
+// totalQuestions init: used to display the total number of questions
+let totalQuestions = questions.length;
+
+//  variables containing the current Question values ==> ready to inject into the DOM elements.
+let questionTitle = "";
+let answers = [];
+let correctAnswer = "";
+let image = "";
+
+
+// ------------------  Functions   ------------------
+
+// Function get question objects values to display the current question items.
+function displayQuestion(index) {
+    const questionItem = questions[index - 1];
+
+    // values 
+    questionTitle = questionItem.question;
+    answers = questionItem.answers;
+    correctAnswer = questionItem.correctAnswer;
+    image = questionItem.image;
+}
+
+
+
+
+
+
+
+
+
+
+// ------------------  View   -----------------
+
+
+// Function call ==> to put inside the click button event listener (keep this argument).
+displayQuestion(currentQuestion);
+console.log("titre de la question :", questionTitle);
+console.log("choix de reponses", answers[0]);
+console.log("reponses de la question :", correctAnswer);
+console.log("image de la question :", image);
